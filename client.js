c56382e3c695554c36f98627c304bdb08843e1ec
symphony <jciurc@gmail.com>
2022-04-13 14:30:54 -0600

    Created play and client files. Able to connect to server

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require('net');
+const config = {
+  host: '165.227.47.243',
+  port: 50541
+};
+
+const connect = () => {
+  const conn = net.createConnection(config);
+  conn.setEncoding('utf8'); // interpret data as text
+
+  conn.on('connect', (data) => {
+    console.log('Connected to server!');
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  conn.on('end', () => {
+    console.log('Disconnected from server.');
+  });
+
+  conn.on('connect', () => {
+    conn.write("Player 1 has joined.");
+  });
+
+  return;
+};
+
+module.exports = {connect};
